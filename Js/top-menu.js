986ab41c9d2724465216fbf757d9ea59db04f472
jhmiante <jhmiante@gmail.com>
2020-11-09 22:22:18 -0300

    Add Index

---
+++ Js/top-menu.js
@@ -0,0 +1,30 @@
+var html = 
+'<nav class="navbar navbar-expand navbar-dark fixed-top bg-dark flex-row-reverse">\n'+  
+'      <div class="navbar-collapse" id="navbarCollapse">\n'+  
+'        <ul class="navbar-nav mr-auto">\n'+  
+'          <li class="nav-item active">\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/">Home <span class="sr-only">(current)</span></a>\n'+  
+'          </li>\n'+  
+'          <li class="nav-item">\n'+  
+'            <a class="nav-link disabled" href="#">HTML</a>\n'+  
+'          </li>\n'+  
+'          <li class="nav-item">\n'+  
+'            <a class="nav-link disabled" href="#">HTML 5</a>\n'+  
+'          </li>\n'+  
+'          <li class="nav-item">\n'+  
+'            <a class="nav-link disabled" href="#">CSS</a>\n'+  
+'          </li>\n'+  
+'          <li class="nav-item">\n'+  
+'            <a class="nav-link disabled" href="#">JavaScript</a>\n'+  
+'          </li>\n'+  
+'          <li class="nav-item">\n'+  
+'            <a class="nav-link disabled" href="#">jQuery</a>\n'+  
+'          </li>\n'+  
+'          <li class="nav-item">\n'+  
+'            <a class="nav-link disabled" href="#">Ajax</a>\n'+  
+'          </li>\n'+  
+'        </ul>\n'+    
+'      </div>\n'+  
+'    </nav>\n'
+
+document.getElementById("top-menu").innerHTML = html;
