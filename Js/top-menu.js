2b4fa7dff3e50e7f590bccecbfd59c919be737d5
jhmiante <jhmiante@gmail.com>
2020-11-14 19:55:26 -0300

    att link

--- Js/top-menu.js
+++ Js/top-menu.js
@@ -3,25 +3,25 @@ var html =
 '      <div class="navbar-collapse" id="navbarCollapse">\n'+  
 '        <ul class="navbar-nav mr-auto">\n'+  
 '          <li class="nav-item active">\n'+  
-'            <a class="nav-link" href="https://jhmiante.github.io/">Home <span class="sr-only">(current)</span></a>\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/cs-web/">Home <span class="sr-only">(current)</span></a>\n'+  
 '          </li>\n'+  
 '          <li class="nav-item">\n'+  
-'            <a class="nav-link" href="https://jhmiante.github.io/Pages/001-Fundamentos-HTML.html">HTML</a>\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/cs-web/Pages/001-Fundamentos-HTML.html">HTML</a>\n'+  
 '          </li>\n'+  
 '          <li class="nav-item">\n'+  
-'            <a class="nav-link" href="https://jhmiante.github.io/Pages/002-HTML-5.html">HTML 5</a>\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/cs-web/Pages/002-HTML-5.html">HTML 5</a>\n'+  
 '          </li>\n'+  
 '          <li class="nav-item">\n'+  
-'            <a class="nav-link" href="https://jhmiante.github.io/Pages/003-CSS.html">CSS</a>\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/cs-web/Pages/003-CSS.html">CSS</a>\n'+  
 '          </li>\n'+  
 '          <li class="nav-item">\n'+  
-'            <a class="nav-link" href="https://jhmiante.github.io/Pages/004-JavaScript.html">JavaScript</a>\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/cs-web/Pages/004-JavaScript.html">JavaScript</a>\n'+  
 '          </li>\n'+  
 '          <li class="nav-item">\n'+  
-'            <a class="nav-link" href="https://jhmiante.github.io/Pages/005-jQuery.html">jQuery</a>\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/cs-web/Pages/005-jQuery.html">jQuery</a>\n'+  
 '          </li>\n'+  
 '          <li class="nav-item">\n'+  
-'            <a class="nav-link" href="https://jhmiante.github.io/Pages/006-Ajax.html">Ajax</a>\n'+  
+'            <a class="nav-link" href="https://jhmiante.github.io/cs-web/Pages/006-Ajax.html">Ajax</a>\n'+  
 '          </li>\n'+
 '		   <li class="nav-item">\n'+  
 '            <a class="nav-link" href="https://github.com/jhmiante/WEB/tree/main/CST%20-%20An%C3%A1lise%20e%20Desenvolvimento%20de%20Sistemas">GitHub Web</a>\n'+  
